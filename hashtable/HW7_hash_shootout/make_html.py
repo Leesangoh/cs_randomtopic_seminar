f2bc634f6484679d96f548672f63fde9440cc4b0
Leesangoh <68114701+Leesangoh@users.noreply.github.com>
2022-12-08 01:41:19 +0900

    Delete hashtable directory

--- hashtable/HW7_hash_shootout/make_html.py
+++
@@ -1,5 +0,0 @@
-import sys
-f = open('charts-template.html', 'r')
-html_template = f.read()
-f_2 = open('charts.html', 'w')
-f_2.write(html_template.replace('__CHART_DATA_GOES_HERE__', sys.stdin.read()))
